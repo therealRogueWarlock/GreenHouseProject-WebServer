e63876c258942a8ffe140de2086b7849511ff659
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-11-13 15:29:14 +0100

    Update TempAndHumidity.js

--- WebServer/BBBDriverCall/TempAndHumidity.js
+++ WebServer/BBBDriverCall/TempAndHumidity.js
@@ -2,7 +2,7 @@ import {CommandCaller} from "./CommandCaller.js"
 
 export class TempAndHumidity{
 
-    static execFileName = "./i2ctest";
+    static execFileName = "./WebServer/i2ctest";
     
     static getTemperatureAndHumidity() {
         var out = CommandCaller.systemSync(TempAndHumidity.execFileName);
