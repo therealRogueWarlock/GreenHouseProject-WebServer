31e694a2e01d7dfd361d334d24a4ba95451c5d25
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-11-13 12:11:20 +0100

    SimpleWebserver
    Currently only containing get temperature and humidity socket function

---
+++ WebServer/WebServer.js
@@ -0,0 +1,67 @@
+//Loading modules
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var child_process = require('child_process');
+
+class WebServer {
+    constructor() {
+        this.server;
+    }
+
+    InitServer() {
+
+        if(this.server) return;
+
+        // Initialize the server on port 8888
+        this.server = http.createServer(function (req, res) {
+
+            var file = '.' + ((req.url == '/') ? '/index.html' : req.url);
+            var fileExtension = path.extname(file);
+            var contentType = 'text/html';
+            // If and when css is added to the website
+            
+            if(fileExtension == '.css'){
+                contentType = 'text/css';
+            }
+
+            fs.exists(file, function (exists) {
+                if (exists) {
+                    fs.readFile(file, function (error, content) {
+                        if (!error) {
+                            // Page found, write content
+                            res.writeHead(200, { 'content-type': contentType });
+                            res.end(content);
+                        }
+                    })
+                }
+                else {
+                    // Page not found
+                    res.writeHead(404);
+                    res.end('Page not found');
+                }
+            })
+        }).listen(8888);
+
+        // Loading socket io module
+        var io = require('socket.io')(server);
+
+        // When communication is established
+        io.on('connection', function (socket) {
+            console.log(socket.id);
+            // Service methodes
+            socket.on('getTemperatureAndHumidity', () => {
+
+                socket.emit("returnTemperatureAndHumidity", getTemperatureAndHumidity());
+
+            });
+
+        });
+
+        console.log("Server Running ...");
+        return this.server;
+    }
+}
+
+
+
