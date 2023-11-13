091eb88b4bd81cc82ff36485b6fef8e051391316
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-11-13 20:58:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import {WebServer} from "./WebServer/WebServer.js"
 
 new WebServer().InitServer().StartServer();
 
+let i = 0;
 setInterval(() => {
     console.log('Infinite Loop Test interval n:', i++);
 }, 2000)
