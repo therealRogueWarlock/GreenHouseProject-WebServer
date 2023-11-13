9015b442a3098e6d5cd20a31a087b8fd800bffc4
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-11-13 14:48:59 +0100

    Update WebServer.js

--- WebServer/WebServer.js
+++ WebServer/WebServer.js
@@ -17,7 +17,7 @@ export class WebServer {
 
         // Initialize the server on port 8888
         this.server = http.createServer(function (req, res) {
-            var file = '.' + ((req.url == '/') ? '/WebInterface/index.html' : req.url);
+            var file = './WebInterface' + ((req.url == '/') ? '/WebInterface/index.html' : req.url);
             var fileExtension = path.extname(file);
             var contentType = 'text/html';
             // If and when css is added to the website
@@ -54,7 +54,7 @@ export class WebServer {
     StartServer(){
 
         if(!this.server) return;
-        
+
         var socketIo = new SocketServer(this.server);
         // When communication is established
         socketIo.on('connection', function (socket) {
