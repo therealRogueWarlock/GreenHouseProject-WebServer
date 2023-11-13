a3522558adda5b6e72045471d5b2358e25aaa781
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-11-13 14:30:19 +0100

    moved index file

--- WebServer/WebServer.js
+++ WebServer/WebServer.js
@@ -17,28 +17,33 @@ export class WebServer {
 
         // Initialize the server on port 8888
         this.server = http.createServer(function (req, res) {
-        
-            var file = '' + ((req.url == '/') ? '../WebInterface/index.html' : req.url);
+            console.log(req.url);
+            var file = '.' + ((req.url == '/') ? '/WebInterface/index.html' : req.url);
             console.log(file);
             var fileExtension = path.extname(file);
             var contentType = 'text/html';
             // If and when css is added to the website
-            /*
+            
             if(fileExtension == '.css'){
                 contentType = 'text/css';
             }
-            */
-
-            fs.readFile("index.html", function (error, content) {
-                if (!error) {
-                    console.log("error on load")
-                    // Page found, write content
-                    res.writeHead(200, { 'content-type': contentType });
-                    res.end(content);
-                }else {
+            
+            fs.exists(file, function (exists) {
+                if (exists) {
+                    console.log("file exists")
+                    fs.readFile(file, function (error, content) {
+                        if (!error) {
+                            console.log("error on load")
+                            // Page found, write content
+                            res.writeHead(200, { 'content-type': contentType });
+                            res.end(content);
+                        }
+                    })
+                }
+                else {
                     // Page not found
                     res.writeHead(404);
-                    console.log("file does not exist")
+                    console.log("file does not exists")
                     res.end('Page not found!!!');
                     
                 }
